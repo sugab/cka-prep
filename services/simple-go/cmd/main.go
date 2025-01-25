0274a2b74990e82de871114cc6d9916c8a56be26
bagus.cahyono <baguscah77@gmail.com>
2025-01-25 19:05:24 +0700

    feat: add simple-go apps

---
+++ services/simple-go/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	srv := http.NewServeMux()
+	srv.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		res := Response{
+			Message: "Success",
+		}
+
+		err := json.NewEncoder(w).Encode(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	fmt.Println("Server is running on port 8080...")
+	err := http.ListenAndServe(":8080", srv)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ services/simple-go/go.mod
@@ -0,0 +1,3 @@
+module sugab/simple-go
+
+go 1.22.1
